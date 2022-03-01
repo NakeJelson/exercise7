69e1e90de45285004a5a43835fc65724e3672c4a
NakeJelson <nakejelson78@gmail.com>
2022-03-01 14:12:15 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Contact contact1 = new Contact("Joe Pesci", "joepesci@aol.com");
+
+        BusinessContact bContact1 = new BusinessContact("Macaulay Culkin", "kevin.mccallister@hotmail.com", "555-554-3234");
+
+        contact1.display();
+        bContact1.display();
+    }
+}
+
+//Jake Nelson 3-1-22 exercise7
